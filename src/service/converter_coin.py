915f017bd7dc3437ee48865debf09ab142fd48e6
jackteruya <jack.teruya@gmail.com>
2024-03-21 14:43:08 -0400

    add servece to converter coin

---
+++ src/service/converter_coin.py
@@ -0,0 +1,55 @@
+from src.domain.coins import Coins
+from src.interface.coin_api_repository_interface import CoinApiRepositoryInterface
+from src.interface.cripto_api_repository_interface import CriptoApiRepositoryInterface
+
+
+class ConversorCoin:
+    def __init__(
+        self,
+        coins_api_repository: CoinApiRepositoryInterface,
+        criptos_api_repository: CriptoApiRepositoryInterface,
+    ):
+        self._coin_api = coins_api_repository
+        self._cripto_api = criptos_api_repository
+        self._from = None
+        self._to = None
+
+    def execute(self, amount: float):
+        if self.validation_param():
+            return "Error: the to and from parameters are required"
+        coin = self.treat_data(self._from)
+        return amount * coin.__getattribute__(self._to)
+
+    def set_from(self, value: str):
+        self._from = value
+
+    def set_to(self, value: str):
+        self._to = value
+
+    def validation_param(self):
+        if self._from is None or self._to is None:
+            return False
+
+    def get_data_coins(self, base: str = "USD", date=None):
+        return self._coin_api.get_coin(base, date)
+
+    def get_data_criptos(self):
+        return self._cripto_api.get_cripto()
+
+    def treat_data(self, base_coin: str):
+        base_coin = str(base_coin).lower()
+        coin_api = self.get_data_coins()
+        cripto_api = self.get_data_criptos()
+        coin = Coins()
+        coin.usd = coin_api.usd
+        coin.brl = coin_api.brl
+        coin.eur = coin_api.eur
+        coin.btc = coin_api.__getattribute__(str(coin_api.base).lower()) / (
+            cripto_api.btc / coin_api.__getattribute__(str(cripto_api.base).lower())
+        )
+        coin.eth = coin_api.__getattribute__(str(coin_api.base).lower()) / (
+            cripto_api.eth / coin_api.__getattribute__(str(cripto_api.base).lower())
+        )
+        if coin.__getattribute__(base_coin) != 1.0:
+            coin.update_base_coins(base_coin)
+        return coin
---
+++ tests/integration_test.py
@@ -0,0 +1,35 @@
+from src.repository.cripto_info.cripto_info_repository import CriptoInfoAPIRepository
+from src.repository.vatcomply_api.coin_api_repository import CoinAPIRepository
+from src.service.converter_coin import ConversorCoin
+
+
+def test_converter_return_float():
+    coin_api = CoinAPIRepository()
+    cripto_api = CriptoInfoAPIRepository()
+    from_ = "usd"
+    to_ = "brl"
+    value = 5.00
+    conversor = ConversorCoin(coin_api, cripto_api)
+    conversor.set_from(from_)
+    conversor.set_to(to_)
+    new_value = conversor.execute(value)
+    assert type(new_value) is float
+
+
+def test_converter_to_brl_and_converter_to_usd():
+    coin_api = CoinAPIRepository()
+    cripto_api = CriptoInfoAPIRepository()
+
+    from_ = "usd"
+    to_ = "brl"
+    value = 5.00
+
+    conversor = ConversorCoin(coin_api, cripto_api)
+    conversor.set_from(from_)
+    conversor.set_to(to_)
+    new_value = conversor.execute(value)
+
+    conversor.set_from(to_)
+    conversor.set_to(from_)
+    other_value = conversor.execute(new_value)
+    assert other_value == value
